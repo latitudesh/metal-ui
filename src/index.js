566ca5c8b0ff93311e8c462233e62bff297c2e25
Eduardo Alberto <ebsalberto@users.noreply.github.com>
2020-05-20 00:30:01 -0300

    Skeleton component

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ export { default as Dropdown } from "./Dropdown";
 export { default as Button } from "./Button";
 export { default as Box } from "./Box";
 export { default as Menu } from "./Menu";
+export { default as Skeleton } from "./Skeleton";
 export * from "./Flags";
 export * from "./OperatingSystems";
