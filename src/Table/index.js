82a6d9687b80aef021d8d7ca0bc56e1a9f2e0072
Eduardo Alberto <ebsalberto@users.noreply.github.com>
2020-05-21 16:45:36 -0300

    Table to 8-point

--- src/Table/index.js
+++ src/Table/index.js
@@ -17,7 +17,7 @@ Table.Body = ({ children, className }) => (
 Table.HeaderCell = ({ children, className }) => (
   <th
     className={classNames(
-      "px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider",
+      "px-6 py-2 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider",
       className
     )}
   >
