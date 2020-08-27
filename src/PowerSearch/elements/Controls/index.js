3082c9d1385dfe01c59565d1739241674d29ab5b
Jarrod Flesch <jarrodmflesch@gmail.com>
2020-08-27 18:37:42 -0400

    adds arrow svgs and a few alert catches

--- src/PowerSearch/elements/Controls/index.js
+++ src/PowerSearch/elements/Controls/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
+
+import ArrowDown from '../../assets/ArrowDown';
+import ArrowUp from '../../assets/ArrowUp';
+
 import styles from './style';
 
 const Controls = () => (
   <div
-    className="sticky bottom-0 p-3 bg-white"
-    style={{ boxShadow: styles.boxShadow }}
+    className="sticky bottom-0 p-3 bg-white rounded-b-md"
+    style={{ ...styles.controlBar }}
     >
-      Keyboard Control explainations will go here
+      <span className="mr-2 rounded-sm text-gray-400" style={{...styles.controlKey}}>
+        <ArrowDown />
+      </span>
+      
+      <span className="mr-2 rounded-sm text-gray-400" style={{...styles.controlKey}}>
+        <ArrowUp />
+      </span>
+      <small className="text-gray-400">to navigate</small>
   </div>
 );
 
