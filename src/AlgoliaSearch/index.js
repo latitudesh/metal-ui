fb289d02ae7be63191ce93489174f97a143e0e1b
Jarrod Flesch <jarrodmflesch@gmail.com>
2020-09-17 15:02:58 -0400

    moves the props expected up to the root component

--- src/AlgoliaSearch/index.js
+++ src/AlgoliaSearch/index.js
@@ -29,6 +29,23 @@ const AlgoliaSearch = (props) => {
 AlgoliaSearch.propTypes = {
   ALGOLIA_APP_ID: PropTypes.string.isRequired,
   ALGOLIA_API_SEARCH_KEY: PropTypes.string.isRequired,
+  specialChar: PropTypes.string.isRequired,
+  searchOperators: PropTypes.arrayOf(PropTypes.string).isRequired,
+  indices: PropTypes.arrayOf(PropTypes.shape({
+    indexName: PropTypes.string,
+    displayName: PropTypes.string,
+    renderCardInfo: PropTypes.func,
+    formatHitURL: PropTypes.func,
+    searchConditions: PropTypes.arrayOf(
+      PropTypes.shape({
+        conditionType: PropTypes.oneOf(['OR', 'AND']),
+        conditionString: PropTypes.string,
+      }),
+    )
+  })).isRequired,
+  scrollWindowHeight: PropTypes.number,
+  customLoader: PropTypes.node,
+  customNoResults: PropTypes.node,
 };
 
 export default AlgoliaSearch;
