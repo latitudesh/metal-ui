f32696a7e0459bf6c967efce01fe0338b5ed906d
robsoncoelho <robson.amc@gmail.com>
2020-09-22 14:23:40 -0300

    Fix path into preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { addDecorator } from '@storybook/react';
+import React from "react";
+import { addDecorator } from "@storybook/react";
 import { configure } from "@storybook/react";
 import "../styles/index.css";
 
 // automatically import all story.js files
-const req = require.context("../", true, /story\.js$/);
+const req = require.context("../src", true, /story\.js$/);
 
 function loadStories() {
   req.keys().forEach(req);
