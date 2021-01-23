08e3901bdb7a733febe6fa8364d256c9073adb75
Eduardo Alberto <eduardo@maxihost.com.br>
2021-01-23 01:04:01 -0300

    feat: Init

---
+++ src/DropdownMenu/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import * as DropdownPrimitive from "@radix-ui/react-dropdown-menu";
+
+export const Dropdown = DropdownPrimitive.Root;
+export const DropdownTrigger = DropdownPrimitive.Trigger;
+export const DropdownContent = DropdownPrimitive.Content;
+export const DropdownLabel = DropdownPrimitive.Label;
+export const DropdownItem = DropdownPrimitive.Item;
+export const DropdownGroup = DropdownPrimitive.Group;
+export const DropdownCheckboxItem = DropdownPrimitive.CheckboxItem;
+export const DropdownItemIndicator = DropdownPrimitive.ItemIndicator;
+export const DropdownRadioGroup = DropdownPrimitive.RadioGroup;
+export const DropdownRadioItem = DropdownPrimitive.RadioItem;
+export const DropdownSeparator = DropdownPrimitive.Separator
+export const DropdownArrow = DropdownPrimitive.Arrow;
