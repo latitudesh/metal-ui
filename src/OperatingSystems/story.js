6b1a3ff7d2da07799726df91958ad6db64d670c7
Eduardo Alberto <eduardo@maxihost.com.br>
2021-02-26 17:19:41 -0300

    chore: Removes OperatingSystems component

--- src/OperatingSystems/story.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import CentOS from "./CentOS";
-
-storiesOf("OperatingSystem", module)
-  .add("size props", () => <CentOS size={128} />)
-  .add("CentOS", () => <CentOS />);
