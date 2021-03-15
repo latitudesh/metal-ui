56d0ec92c0bcc580db8e6b7dafcca54debbea41d
Eduardo Alberto <eduardo@maxihost.com.br>
2021-03-15 16:38:15 -0300

    Revert "fix: Storybook addon order"
    This reverts commit 8171e66e7698e4c7b363afa503af016b496c66ff.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,12 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-controls",
     {
       name: "@storybook/addon-docs",
       options: {
@@ -9,9 +15,6 @@ module.exports = {
         sourceLoaderOptions: null,
       },
     },
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-controls",
     "@storybook/addon-actions/register",
   ],
 };
