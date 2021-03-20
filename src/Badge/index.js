40825b05798b3e62d72967e510636efd10c12ee9
Eduardo Alberto <eduardo@maxihost.com.br>
2021-03-20 04:57:33 -0300

    chore: Badge rounded style and color update

--- src/Badge/index.js
+++ src/Badge/index.js
@@ -1,36 +1,43 @@
-import React from 'react'
-import classNames from 'classnames'
-import Text from '../Typography/Text'
+import React from "react";
+import classNames from "classnames";
 
-const Badge = ({ style, children, minimal, className }) => {
+const Badge = ({ style, children, minimal, className, rounded }) => {
   let badgeStyle = [
     {
-      'bg-green-100 text-green-800': style === 'positive',
-      'bg-yellow-100 text-yellow-800': style === 'warning',
-      'bg-blue-100 text-blue-800': style === 'attention',
-      'bg-red-100 text-red-800': style === 'danger',
-      'bg-gray-100 text-gray-800': style === 'neutral',
+      "text-success": style === "positive",
+      "text-warning": style === "warning",
+      "text-violet": style === "attention",
+      "text-error": style === "danger",
     },
-  ]
+  ];
+
+  let isRounded = [
+    {
+      "rounded-full bg-foreground text-white": rounded,
+    },
+  ];
 
   return (
     <div
       className={classNames(
-        'inline-flex items-center px-2.5 rounded-md text-sm font-medium h-6 leading-6',
+        "border border-border inline-flex items-center px-2.5 rounded-md text-sm font-medium h-6 leading-6",
         badgeStyle,
+        isRounded,
         className
-      )}>
+      )}
+    >
       {!minimal && (
         <svg
-          className={classNames('-ml-0.5 mr-1.5 h-2 w-2', badgeStyle)}
+          className={classNames("-ml-0.5 mr-1.5 h-2 w-2", badgeStyle)}
           fill="currentColor"
-          viewBox="0 0 8 8">
+          viewBox="0 0 8 8"
+        >
           <circle cx="4" cy="4" r="3" />
         </svg>
       )}
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default Badge
+export default Badge;
