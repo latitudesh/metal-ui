300e89f7a72e29e3fa1c9460637f238a679a21d2
Daniel Nadasan <n23daniel@gmail.com>
2021-03-25 17:58:41 +0200

    fix: Fixes Emotion import in Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,7 @@
+const path = require("path")
+
+const toPath = (_path) => path.join(process.cwd(), _path)
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -10,4 +14,19 @@ module.exports = {
     },
     "@storybook/addon-actions/register",
   ],
+  webpackFinal: async (config) => {
+    // TODO: Remove when Storybook adds support for Emotion 11. Fix inspired from here - https://github.com/storybookjs/storybook/issues/13277#issuecomment-751747964
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "@emotion/core": toPath("node_modules/@emotion/react"),
+          "@emotion/styled": toPath("node_modules/@emotion/styled"),
+          "emotion-theming": toPath("node_modules/@emotion/react"),
+        }
+      }
+    };
+  }
 };
