c7bf49863f9b3001da340d83f5bb295be70aa3ee
Eduardo Alberto <eduardo@maxihost.com.br>
2021-04-06 01:47:41 -0300

    feat: Toolbar

--- src/Toolbar/index.js
+++ src/Toolbar/index.js
@@ -1,13 +1,16 @@
 import tw, { styled, css } from "twin.macro";
 import * as ToolbarPrimitive from "@radix-ui/react-toolbar";
 
-const Toolbar = styled(ToolbarPrimitive.Root)(() => [tw`flex`]);
+const Toolbar = styled(ToolbarPrimitive.Root)(() => [
+  tw`flex`,
+  css`
+    margin: 0 -5px;
+  `,
+]);
 
 const itemStyles = css`
-  ${tw`appearance-none bg-transparent border border-border py-1 px-2 my-0 mx-2 shadow overflow-hidden rounded-sm text-sm`}
-  :first-child {
-    ${tw`mx-0`}
-  }
+  ${tw`appearance-none bg-transparent border border-border py-1 px-2 overflow-hidden rounded text-sm`}
+  margin: 0 5px;
 `;
 
 const ToolbarButton = styled(ToolbarPrimitive.Button)(() => [itemStyles]);
@@ -18,11 +21,28 @@ const ToolbarLink = styled(ToolbarPrimitive.Link)(() => [
 ]);
 
 const ToolbarSeparator = styled(ToolbarPrimitive.Separator)(() => [
-  tw`bg-accent-four`,
+  tw`bg-accent-four mx-2 my-1`,
   css`
     width: 1px;
-    margin: 5px;
   `,
 ]);
 
-export { Toolbar, ToolbarButton, ToolbarLink, ToolbarSeparator };
+const ToolbarToggleGroup = ToolbarPrimitive.ToggleGroup;
+
+const ToolbarToggle = styled(ToolbarPrimitive.ToggleItem)(() => [
+  itemStyles,
+  css`
+    &[data-state="on"] {
+      ${tw`bg-accent-three`};
+    }
+  `,
+]);
+
+export {
+  Toolbar,
+  ToolbarButton,
+  ToolbarLink,
+  ToolbarSeparator,
+  ToolbarToggleGroup,
+  ToolbarToggle,
+};
--- src/index.js
+++ src/index.js
@@ -31,4 +31,5 @@ export {
   ToolbarButton,
   ToolbarLink,
   ToolbarSeparator,
+  ToolbarToggle
 } from "./Toolbar";
