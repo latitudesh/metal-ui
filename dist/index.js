fe7e6e7fc7c5296afd30cbaa961007685e718bba
Eduardo Alberto <eduardo@maxihost.com.br>
2021-04-08 00:57:46 -0300

    chore: build

--- dist/index.js
+++ dist/index.js
@@ -6486,10 +6486,10 @@ var Heading_Heading = /*#__PURE__*/function (_PureComponent) {
           "text-3xl font-semibold tracking-tighter": size == 2,
           "text-2xl font-semibold tracking-tight": size == 3,
           "text-xl font-semibold tracking-tight": size == 4,
-          "text-xl font-normal": size == 5,
+          "text-lg font-normal": size == 5,
           "text-normal font-normal": size == 6,
           "text-foreground": color == "default",
-          "text-error": color == "danger"
+          "text-error-dark": color == "danger"
         })
       }, props));
     }
