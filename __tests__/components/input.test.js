83fc07515471e1f289268d476825bddb8770ff5e
Maxwel Dsouza <2565045+maxweldsouza@users.noreply.github.com>
2022-02-28 15:35:16 +0530

    feat: Add test for onChange

--- __tests__/components/input.test.js
+++ __tests__/components/input.test.js
@@ -1,17 +1,25 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom/extend-expect'
 
 import Input from "../../src/Input";
 
 describe("Snapshots/Components/Input", () => {
-  it("Render input", () => {
-    const component = renderer.create(<Input />);
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-  it("Render input with addon", () => {
+  it("Renders input with addon", () => {
     const component = renderer.create(<Input type={'number'} min={0} step={10} max={100} rightAddon={'TB'}/>);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  it('Calls onChange correctly', async () => {
+    const mockOnChange = jest.fn()
+    render(
+      <Input onChange={mockOnChange} label={'Name'}/>
+    )
+
+    userEvent.type(screen.getByLabelText('Name'), 'J');
+    expect(mockOnChange.mock.calls[0][0]).toBe('J')
+    expect(mockOnChange.mock.calls[0][1].type).toBe('change')
   });
 });
