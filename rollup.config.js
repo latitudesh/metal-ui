2e5764c8c1b12519ab113292ba9d4a287f247357
immarcelo <marcelo.sousa@maxihost.com.br>
2022-03-22 12:57:49 -0300

    BREAKING CHANGE:removed deprecated AlgoliaSearch component

--- rollup.config.js
+++ rollup.config.js
@@ -1,73 +1,72 @@
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import { babel } from "@rollup/plugin-babel";
-import { uglify } from "rollup-plugin-uglify";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { babel } from '@rollup/plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === 'production';
 
 const files = [
-  "AlgoliaSearch",
-  "Avatar",
-  "Badge",
-  "Box",
-  "Button",
-  "Content",
-  "DropdownMenu",
-  "Feedback",
-  "Header",
-  "Input",
-  "Select",
-  "Sidesheet",
-  "Skeleton",
-  "SpinningDots",
-  "Table",
-  "Tabs",
-  "Textarea",
-  "Toast",
-  "Typography", // This contains Text, Heading, UnorderedList and ListItem
-  "Text",
-  "Heading",
-  "UnorderedList",
-  "ListItem",
-  "GlobalStyles"
+    'Avatar',
+    'Badge',
+    'Box',
+    'Button',
+    'Content',
+    'DropdownMenu',
+    'Feedback',
+    'Header',
+    'Input',
+    'Select',
+    'Sidesheet',
+    'Skeleton',
+    'SpinningDots',
+    'Table',
+    'Tabs',
+    'Textarea',
+    'Toast',
+    'Typography', // This contains Text, Heading, UnorderedList and ListItem
+    'Text',
+    'Heading',
+    'UnorderedList',
+    'ListItem',
+    'GlobalStyles'
 ];
 
 const options = {
-  external: [
-    "react",
-    "react-dom",
-    "@emotion/styled",
-    "@emotion/react",
-    "classnames",
-    "prop-types",
-  ],
-  plugins: [
-    babel({
-      babelHelpers: "bundled",
-    }),
-    nodeResolve(),
-    commonjs(),
-    isProduction && uglify(),
-  ],
-  output: {
-    exports: "auto",
-    globals: {
-      react: "React",
-      "react-dom": "ReactDOM",
-    },
-    format: "cjs",
-  },
+    external: [
+        'react',
+        'react-dom',
+        '@emotion/styled',
+        '@emotion/react',
+        'classnames',
+        'prop-types'
+    ],
+    plugins: [
+        babel({
+            babelHelpers: 'bundled'
+        }),
+        nodeResolve(),
+        commonjs(),
+        isProduction && uglify()
+    ],
+    output: {
+        exports: 'auto',
+        globals: {
+            react: 'React',
+            'react-dom': 'ReactDOM'
+        },
+        format: 'cjs'
+    }
 };
 
 module.exports = [
-  ...files.map((file) => {
-    return {
-      ...options,
-      input: `src/${file}/index.js`,
-      output: {
-        ...options.output,
-        dir: `dist/${file}`,
-      },
-    };
-  }),
+    ...files.map((file) => {
+        return {
+            ...options,
+            input: `src/${file}/index.js`,
+            output: {
+                ...options.output,
+                dir: `dist/${file}`
+            }
+        };
+    })
 ];
