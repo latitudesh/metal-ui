8a51da93ecf4a075feb85e4d4d0478e91bdbc0a0
Maxwel Dsouza <2565045+maxweldsouza@users.noreply.github.com>
2022-08-15 16:56:41 +0530

    feat: Simplify build

--- rollup.config.js
+++ rollup.config.js
@@ -2,50 +2,22 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { babel } from "@rollup/plugin-babel";
 import { uglify } from "rollup-plugin-uglify";
+import { visualizer } from "rollup-plugin-visualizer";
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const files = [
-  "Avatar",
-  "Badge",
-  "Box",
-  "Button",
-  "Content",
-  "DropdownMenu",
-  "Feedback",
-  "Header",
-  "Input",
-  "Editable",
-  "Label",
-  "Select",
-  "Sidesheet",
-  "Skeleton",
-  "SpinningDots",
-  "Table",
-  "Tabs",
-  "Textarea",
-  "Toast",
-  "Typography", // This contains Text, Heading, UnorderedList and ListItem
-  "Text",
-  "Heading",
-  "UnorderedList",
-  "ListItem",
-  "Tooltip",
-  "RadioGroup",
-  "GlobalStyles"
-];
-
 const options = {
+  input: 'src/index.js',
   external: (name) => {
     const externals = [
-    "react",
-    "react-dom",
-    "@emotion",
-    "classnames",
-    "prop-types",
-    "@radix-ui",
-    "@heroicons/react",
-  ]
+      "react",
+      "react-dom",
+      "@emotion",
+      "classnames",
+      "prop-types",
+      "@radix-ui",
+      "@heroicons/react",
+    ]
     return externals.some(e => name.includes(e))
   },
   plugins: [
@@ -54,27 +26,30 @@ const options = {
     }),
     nodeResolve(),
     commonjs(),
-    isProduction && uglify()
+    isProduction && uglify(),
+    visualizer()
   ],
-  output: {
+  output: [{
     exports: "auto",
+    dir: 'dist',
+    preserveModules: true,
     globals: {
       react: "React",
       "react-dom": "ReactDOM"
     },
     format: "cjs"
-  }
+  },
+    {
+      exports: "auto",
+      dir: 'esm',
+      preserveModules: true,
+      globals: {
+        react: "React",
+        "react-dom": "ReactDOM"
+      },
+      format: "esm"
+    }
+  ]
 };
 
-module.exports = [
-  ...files.map((file) => {
-    return {
-      ...options,
-      input: `src/${file}/index.js`,
-      output: {
-        ...options.output,
-        dir: `dist/${file}`
-      }
-    };
-  })
-];
+module.exports = options
