e00b93cc0077225aad0813e71fe081befc046d28
Eduardo Alberto <eduardo@latitude.sh>
2022-09-19 17:57:10 -0300

    fix(DropdownMenu): Makes open animation faster and removes animation on close

--- src/DropdownMenu/index.js
+++ src/DropdownMenu/index.js
@@ -11,43 +11,34 @@ const DropdownPortal = DropdownPrimitive.Portal;
 const slideDown = keyframes`
   0% {
     opacity: 0; 
-    transform: translateY(-2px)
+    transform: translateY(-0.3125rem);
   }
   to {
     opacity: 1;
-    transform: translateY(0px)
-  }`;
-const slideUp = keyframes`
-  0% {
-    opacity: 1
-  }
-  to {
-    opacity: 0;
-    transform: translateY(-2px);
+    transform: none;
   }`;
 
-const StyledDropdownContent = styled(DropdownPrimitive.Content)(({ animated }) => [
+const StyledDropdownContent = styled(DropdownPrimitive.Content)(() => [
   tw`mt-2 shadow-lg bg-white rounded z-10`,
   css`
     min-width: 150px;
     &[data-state="open"] {
-      animation: ${slideDown} 0.3s cubic-bezier(0, 0, 0.2, 1) forwards;
+      animation: ${slideDown} 0.1s cubic-bezier(0.25, 0.1, 0.25, 1) forwards;
     }
     &[data-state="closed"] {
-      animation: ${slideUp} 0.3s cubic-bezier(0, 0, 0.2, 1) forwards;
+      opacity: 0;
     }
-  `
+  `,
 ]);
 
-const DropdownContent = ({ children, ...rest }) =>
+const DropdownContent = ({ children, ...rest }) => (
   <DropdownPortal>
-    <StyledDropdownContent {...rest} >
-      {children}
-    </StyledDropdownContent>
+    <StyledDropdownContent {...rest}>{children}</StyledDropdownContent>
   </DropdownPortal>
+);
 
 const DropdownLabel = styled(DropdownPrimitive.Label)(() => [
-  tw`px-4 py-2 text-xs text-accent-six uppercase`
+  tw`px-4 py-2 text-xs text-accent-six uppercase`,
 ]);
 
 const DropdownItem = styled(DropdownPrimitive.Item)(() => [
@@ -58,16 +49,16 @@ const DropdownItem = styled(DropdownPrimitive.Item)(() => [
     &[data-disabled] {
       ${tw`text-accent-three cursor-default`}
     }
-  `
+  `,
 ]);
 
 const DropdownSeparator = styled(DropdownPrimitive.Separator)(() => [
-  tw`bg-accent-two border-b border-border`
+  tw`bg-accent-two border-b border-border`,
 ]);
 
 DropdownContent.defaultProps = {
   onCloseAutoFocus: (e) => e.preventDefault(),
-  disableOutsidePointerEvents: false
+  disableOutsidePointerEvents: false,
 };
 
 export {
@@ -76,5 +67,5 @@ export {
   DropdownContent,
   DropdownLabel,
   DropdownItem,
-  DropdownSeparator
+  DropdownSeparator,
 };
