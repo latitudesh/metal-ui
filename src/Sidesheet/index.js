9bac82378633556c0de194bff294752f45720e6d
marcelo-sousa-mh <marcelo.sousa@maxihost.com.br>
2022-09-24 15:15:34 -0300

    fix: sidesheet box classes to twin.macro

--- src/Sidesheet/index.js
+++ src/Sidesheet/index.js
@@ -1,15 +1,15 @@
 import { useState, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
-import PropTypes, { string } from "prop-types";
-import classNames from "classnames";
+import PropTypes from "prop-types";
 import Box from "../Box";
 import { useKeyPressEvent } from "react-use";
+import tw from "twin.macro";
 
 function XIcon() {
   return (
     <svg
       fill="none"
-      className="text-accent-four"
+      tw="text-accent-four"
       stroke="currentColor"
       viewBox="0 0 24 24"
       width="16px"
@@ -81,7 +81,7 @@ const Sidesheet = ({
     if (isOpened && transition) {
       onClose();
       setTransition(false);
-      timeout = setTimeout(() => setIsOpened(false), 500);
+      timeout = setTimeout(() => setIsOpened(false), 300);
     }
     return () => clearTimeout(timeout)
   };
@@ -93,79 +93,77 @@ const Sidesheet = ({
       {children}
       {isOpened && (
         <SidesheetContent id="sidesheet">
-          <div
-            onClick={() => closeTransition()}
-            className={classNames(
-              "fixed z-50 inset-0 opacity-25 duration-200 delay-100 transition",
-              {
-                "bg-accent-eight": transition,
-                "bg-transparent": !transition,
-              }
-            )}
-          />
-          <div
-            ref={portal}
-            style={{
-              transition: "transform .2s cubic-bezier(.3,0,0,1)",
-              transform: transition
-                ? `translateX(calc(100vw - ${width}px - 20px))`
-                : "translateX(100vw)",
-              top: 0,
-              bottom: 0,
-              width: width,
-              height: "calc(100% - 20px)",
-            }}
-            className={
-              "fixed z-50 min-w-0 bg-white duration-300 delay-200 h-full flex flex-col shadow-xl m-2 rounded"
-            }
-          >
-            <Box
-              flex
-              alignItems="center"
-              className="relative border-b border-border rounded rounded-b-none"
+            <div
+              onClick={isOpened ?  () => closeTransition() : null}
+              css={[
+                tw`fixed z-50 inset-0 opacity-25 duration-200 delay-100 transition`,
+                transition && tw`bg-accent-eight`,
+                !transition && tw`bg-transparent`,
+              ]}
+            />
+            <div
+              ref={portal}
+              style={{
+                transition: "transform .2s cubic-bezier(.3,0,0,1)",
+                transform: transition
+                  ? `translateX(0)`
+                  : "translateX(100%)",
+                top: 0,
+                bottom: 0,
+                right:0,
+                width: width,
+                maxWidth: "calc(100vw - 20px)",
+                height: "calc(100% - 20px)",
+              }}
+              tw="fixed z-50 min-w-0 bg-white duration-300 delay-200 h-full flex flex-col shadow-xl m-2 rounded"
             >
-              <div className="text-foreground flex-auto truncate leading-6">
-                {title}
-              </div>
               <Box
                 flex
-                backgroundColor="transparent"
-                className="cursor-pointer bg-transparent"
-                noPadding
+                alignItems="center"
+                tw="relative border-b border-border rounded rounded-b-none"
               >
-                <div onClick={() => closeTransition()}>
-                  <XIcon />
+                <div tw="text-foreground flex-auto truncate leading-6">
+                  {title}
                 </div>
+                <Box
+                  flex
+                  backgroundColor="transparent"
+                  tw="cursor-pointer bg-transparent"
+                  noPadding
+                >
+                  <div onClick={() => closeTransition()}>
+                    <XIcon />
+                  </div>
+                </Box>
               </Box>
-            </Box>
-            <Box
-              flex
-              flexDirection="col"
-              overflow={"overflow-y-auto"}
-              className="sidesheet-content relative flex-1 rounded px-3 py-3"
-              noPadding
-            >
               <Box
-                noPadding
                 flex
                 flexDirection="col"
                 overflow={"overflow-y-auto"}
-                className="overflow-visible px-3 py-3"
+                tw="relative flex-1 rounded px-3 py-3"
+                noPadding
               >
-               <div className="flex-shrink-0">{content}</div>
+                <Box
+                  noPadding
+                  flex
+                  flexDirection="col"
+                  overflow={"overflow-y-auto"}
+                  tw="overflow-visible px-3 py-3"
+                >
+                 <div tw="flex-shrink-0">{content}</div>
+                </Box>
               </Box>
-            </Box>
-            {action && (
-              <Box
-                flex
-                justifyContent="evenly"
-                alignItems="center"
-                className="sidesheet-action h-16 border-t border-border relative flex-initial rounded rounded-t-none"
-              >
-                {action}
-              </Box>
-            )}
-          </div>
+              {action && (
+                <Box
+                  flex
+                  justifyContent="evenly"
+                  alignItems="center"
+                  tw="h-16 border-t border-border relative flex-initial rounded rounded-t-none"
+                >
+                  {action}
+                </Box>
+              )}
+            </div> 
         </SidesheetContent>
       )}
     </div>
